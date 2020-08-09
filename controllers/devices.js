8330a3b2a65a3e7fa92c6f5869a9b0e5d2aef594
Christian Stewart <heeler_cs@hotmail.com>
2020-08-09 16:09:08 -0500

    Added get single device route

--- controllers/devices.js
+++ controllers/devices.js
@@ -4,7 +4,7 @@ const Device = require('../models/Device')
 
 // @desc     Get all devices
 // @route    GET /api/v1/devices
-// @access   Private, Users, Admins
+// @access   Private, Users & Admins
 exports.getDevices = asyncHandler(async (req, res, next) => {
 	let query
 
@@ -26,6 +26,30 @@ exports.getDevices = asyncHandler(async (req, res, next) => {
 	})
 })
 
+// @desc     Get single device
+// @route    GET /api/v1/devices/:id
+// @access   Private, Users & Admins
+exports.getDevice = asyncHandler(async (req, res, next) => {
+	const device = await Device.findById(req.params.id)
+
+	if (!device) {
+		return next(
+			new ErrorResponse(
+				`Resource not found with id ${req.params.id}`,
+				404
+			)
+		)
+	}
+
+	res.status(200).json({
+		success: true,
+		data: device,
+	})
+})
+
+// @desc     Create device
+// @route    POST /api/v1/devices
+// @access   Private, Admins
 exports.createDevice = asyncHandler(async (req, res, next) => {
 	const device = await Device.create(req.body)
 
--- routes/devices.js
+++ routes/devices.js
@@ -1,6 +1,10 @@
 const express = require('express')
 
-const { getDevices, createDevice } = require('../controllers/devices')
+const {
+	getDevices,
+	getDevice,
+	createDevice,
+} = require('../controllers/devices')
 
 const router = express.Router()
 
@@ -8,7 +12,11 @@ const { protect, authorize } = require('../middleware/auth')
 
 // prettier-ignore
 router.route('/')
-	.get(protect, authorize('user', 'admin', 'superadmin'), getDevices)
+	.get(protect, getDevices)
 	.post(protect, authorize('admin', 'superadmin'), createDevice)
+
+// prettier-ignore
+router.route('/:id')
+	.get(protect, getDevice)
 
 module.exports = router
