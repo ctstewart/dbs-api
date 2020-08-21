da40bead0f06fde47403dc2a556ec98112de13a8
Christian Stewart <heeler_cs@hotmail.com>
2020-08-21 10:09:44 -0500

    Moved advanced results to middleware

--- controllers/logs.js
+++ controllers/logs.js
@@ -1,80 +1,11 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
-
-// Models
 const Log = require('../models/Log')
 
+// @desc     Get all logs
+// @route    GET /api/v1/logs
+// @route    GET /api/v1/users/:userId/logs
+// @access   Private, Admins
 exports.getLogs = asyncHandler(async (req, res, next) => {
-	let query
-
-	// Copy req.query
-	const reqQuery = { ...req.query }
-
-	// Fields to exclude
-	const removeFields = ['select', 'sort', 'page', 'limit']
-
-	// Loop over removeFields and delete them from reqQuery
-	removeFields.forEach((param) => delete reqQuery[param])
-
-	// Create query string
-	let queryStr = JSON.stringify(reqQuery)
-
-	// Create operators ($gt, $gte, etc.)
-	queryStr = queryStr.replace(
-		/\b(gt|gte|lt|lte|in)\b/g,
-		(match) => `$${match}`
-	)
-
-	// Finding resource
-	query = Log.find(JSON.parse(queryStr))
-
-	// Select Fields
-	if (req.query.select) {
-		const fields = req.query.select.split(',').join(' ')
-		query = query.select(fields)
-	}
-
-	// Sort
-	if (req.query.sort) {
-		const sortBy = req.query.sort.split(',').join(' ')
-		query = query.sort(sortBy)
-	} else {
-		query = query.sort('manufacturer fullRetail')
-	}
-
-	// Pagination
-	const page = parseInt(req.query.page, 10) || 1
-	const limit = parseInt(req.query.limit, 10) || 25
-	const startIndex = (page - 1) * limit
-	const endIndex = page * limit
-	const total = await Log.countDocuments(JSON.parse(queryStr))
-
-	query = query.skip(startIndex).limit(limit)
-
-	// Executing query
-	const logs = await query
-
-	// Pagination result
-	const pagination = {}
-
-	if (endIndex < total) {
-		pagination.next = {
-			page: page + 1,
-			limit,
-		}
-	}
-
-	if (startIndex > 0) {
-		pagination.prev = {
-			page: page - 1,
-			limit,
-		}
-	}
-
-	res.status(200).json({
-		success: true,
-		count: logs.length,
-		pagination,
-		data: logs,
-	})
+	res.status(200).json(res.advancedResults)
 })
