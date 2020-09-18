946b7064c4f6827bdad8017289e06bd490e4fb66
Christian Stewart <heeler_cs@hotmail.com>
2020-09-17 19:33:54 -0500

    Forgot to add "new" to the ErrorResponse class

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,11 +59,10 @@ exports.updatePassword = asyncHandler(async (req, res, next) => {
 	const user = await User.findById(req.user.id).select('+password')
 
 	if (!(await user.matchPassword(req.body.currentPassword))) {
-		return next(ErrorResponse('Password is incorrect', 401))
+		return next(new ErrorResponse('Password is incorrect', 401))
 	}
 
 	user.password = req.body.newPassword
-
 	await user.save()
 
 	sendTokenResponse(user, 200, res)
