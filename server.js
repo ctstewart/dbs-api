b03fd670648e5ee43a16f7281db83b8d9e6af468
Christian Stewart <heeler_cs@hotmail.com>
2020-09-19 15:24:50 -0500

    Added middleware for security

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
@@ -26,6 +32,28 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Sanitize data
+app.use(mongoSanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xss())
+
+// Rate limiting
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 10 minutes
+	max: 100,
+})
+app.use(limiter)
+
+// Prevent http param pollution
+app.use(hpp())
 
 // Mount routers
 app.use('/api/v1/devices', devices)
