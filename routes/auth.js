ee6e13d272d8daa00a29b4d65563604250922edb
Christian Stewart <heeler_cs@hotmail.com>
2020-10-02 13:55:35 -0500

    Added a logout route that clears jwt cookie

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {
 	login,
 	getMe,
+	logout,
 	updatePassword,
 	forgotPassword,
 	resetPassword,
@@ -13,6 +14,7 @@ const { protect } = require('../middleware/auth')
 
 router.post('/login', login)
 router.get('/me', protect, getMe)
+router.get('/logout', protect, logout)
 router.put('/updatepassword', protect, updatePassword)
 router.post('/forgotpassword', forgotPassword)
 router.put('/resetpassword/:resettoken', resetPassword)
