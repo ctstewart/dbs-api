e54277e8fe3810dd960ebcf345ea9b9765ee6ba9
Christian Stewart <heeler_cs@hotmail.com>
2020-10-02 13:55:48 -0500

    Added a comment

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,6 +17,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 		token = req.cookies.token
 	}
 
+	// Make sure token exists
 	if (!token) {
 		return next(
 			new ErrorResponse('Not authorized to access this route', 401)
