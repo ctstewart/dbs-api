0c50038715a6f93629b3d1ef72f4cc0d9b511e13
Christian Stewart <heeler_cs@hotmail.com>
2021-02-02 17:04:22 -0600

    Added support for a new email service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,14 +1,28 @@
 const nodemailer = require('nodemailer')
 
 const sendEmail = async (options) => {
-	const transporter = nodemailer.createTransport({
-		host: process.env.SMTP_HOST,
-		port: process.env.SMTP_PORT,
-		auth: {
-			user: process.env.SMTP_EMAIL,
-			pass: process.env.SMTP_PASSWORD,
-		},
-	})
+	const transportFunction = () => {
+		if (process.env.NODE_ENV === 'production') {
+			return nodemailer.createTransport({
+				service: process.env.SMTP_SERVICE,
+				auth: {
+					user: process.env.SMTP_EMAIL,
+					pass: process.env.SMTP_PASSWORD,
+				},
+			})
+		} else {
+			return nodemailer.createTransport({
+				host: process.env.SMTP_HOST,
+				port: process.env.SMTP_PORT,
+				auth: {
+					user: process.env.SMTP_EMAIL,
+					pass: process.env.SMTP_PASSWORD,
+				},
+			})
+		}
+	}
+
+	const transporter = transportFunction()
 
 	// send mail with defined transport object
 	const message = {
