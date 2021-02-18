587a743a8b7df37de44cba32e3403d6ccb048a21
Christian Stewart <heeler_cs@hotmail.com>
2021-02-18 17:33:26 -0600

    Updated the login route to use the new log model

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,7 +36,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 	}
 
 	if (user.role === 'user') {
-		await Log.create({ category: 'login', user: user.id })
+		await Log.create({ category: 'login', user })
 	}
 
 	sendTokenResponse(user, 200, res)
