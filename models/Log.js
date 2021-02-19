a371edd519641b22bdcd5ae2abb2fab9c3fe1534
Christian Stewart <heeler_cs@hotmail.com>
2021-02-18 18:41:21 -0600

    Changed the structure againthe mongoose query isn't working

--- models/Log.js
+++ models/Log.js
@@ -7,37 +7,35 @@ const LogSchema = new mongoose.Schema(
 			required: [true, 'Please add a category'],
 			enum: ['login'],
 		},
-		user: {
-			firstName: {
-				type: String,
-				required: [true, 'Please add a first name'],
-			},
-			lastName: {
-				type: String,
-				required: [true, 'Please add a last name'],
-			},
-			email: {
-				type: String,
-				required: [true, 'Please add an email'],
-				unique: true,
-				match: [
-					/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-					'Please add a valid email',
-				],
-			},
-			role: {
-				type: String,
-				enum: ['user', 'admin', 'superadmin'],
-				default: 'user',
-			},
-			store: {
-				type: String,
-				required: [true, 'Please include the store'],
-			},
-			district: {
-				type: String,
-				required: [true, 'Please include the district'],
-			},
+		firstName: {
+			type: String,
+			required: [true, 'Please add a first name'],
+		},
+		lastName: {
+			type: String,
+			required: [true, 'Please add a last name'],
+		},
+		email: {
+			type: String,
+			required: [true, 'Please add an email'],
+			unique: true,
+			match: [
+				/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+				'Please add a valid email',
+			],
+		},
+		role: {
+			type: String,
+			enum: ['user', 'admin', 'superadmin'],
+			default: 'user',
+		},
+		store: {
+			type: String,
+			required: [true, 'Please include the store'],
+		},
+		district: {
+			type: String,
+			required: [true, 'Please include the district'],
 		},
 	},
 	{ timestamps: true }
